de3d5202832e832dd03dfd83ff24418bf00e75fa
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-24 22:46:06 +0600

    added icons at the banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,7 +4,8 @@ const Banner = () => {
       <div className="md:flex justify-between items-center">
         <div className="md:w-1/2">
           <h3 className=" text-3xl font-semibold">
-            Hello, I&apos;m Md. <span className="text-[#1AC0EC]">Emran Hossain</span>
+            Hello, I&apos;m Md.{" "}
+            <span className="text-[#1AC0EC]">Emran Hossain</span>
           </h3>
           <p className="text-lg font-semibold">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
@@ -12,6 +13,43 @@ const Banner = () => {
             culpa aliquam illum nemo dolorum explicabo modi temporibus quis!
             Iure, beatae.
           </p>
+          <ul className="flex gap-5 mt-10">
+            <li className="w-10">
+              <a
+                href="https://www.flaticon.com/free-icons/linkedin"
+                title="linkedin icon"
+              >
+                <img src="https://i.ibb.co/1qwMjsT/linkedin-1.png" alt="" />
+              </a>
+            </li>
+
+            <li className="w-10">
+              <a
+                href="https://www.flaticon.com/free-icons/linkedin"
+                title="twitter icon"
+              >
+                <img src="https://i.ibb.co/xLTp5Zw/twitter-2.png" alt="" />
+              </a>
+            </li>
+
+            <li className="w-10">
+              <a
+                href="https://www.flaticon.com/free-icons/linkedin"
+                title="github icon"
+              >
+                <img src="https://i.ibb.co/FmxMcn6/github.png" alt="" />
+              </a>
+            </li>
+
+            <li className="w-10">
+              <a
+                href="https://www.flaticon.com/free-icons/linkedin"
+                title="github icon"
+              >
+                <img src="https://i.ibb.co/HdwsW2t/medium.png" alt="" />
+              </a>
+            </li>
+          </ul>
         </div>
         <div>
           <img
