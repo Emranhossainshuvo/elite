8947b214615a99bc8e9d67486bfbc702f826579a
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-24 22:17:52 +0600

    basic left side banner added and setted in the home page

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+const Banner = () => {
+  return (
+    <>
+      <div>
+        <div><h3 className=" text-3xl font-semibold">Hello, I'm Md. <span className="text-[#1AC0EC]">Emran Hossain</span></h3></div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../../Components/Banner/Banner";
 
 const Home = () => {
     return (
-        <div>
+        <div className="mt-12">
+            <Banner></Banner>
             <h3>This is home page</h3>
         </div>
     );
