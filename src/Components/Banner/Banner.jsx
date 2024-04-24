e4ad69c0a9b674b617f135f33b953826b73fcd56
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-24 22:25:51 +0600

    added picture and texts in the banner of home file

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,9 +1,24 @@
 const Banner = () => {
   return (
     <>
-      <div>
-        <div><h3 className=" text-3xl font-semibold">Hello, I'm Md. <span className="text-[#1AC0EC]">Emran Hossain</span></h3></div>
-        <div></div>
+      <div className="md:flex justify-between items-center">
+        <div className="md:w-1/2">
+          <h3 className=" text-3xl font-semibold">
+            Hello, I&apos;m Md. <span className="text-[#1AC0EC]">Emran Hossain</span>
+          </h3>
+          <p className="text-lg font-semibold">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+            nobis quasi a suscipit eaque non, vero reiciendis harum sapiente
+            culpa aliquam illum nemo dolorum explicabo modi temporibus quis!
+            Iure, beatae.
+          </p>
+        </div>
+        <div>
+          <img
+            src="https://i.ibb.co/9VtwWnR/IMG-20231209-080753-removebg-removebg-preview.png"
+            alt=""
+          />
+        </div>
       </div>
     </>
   );
