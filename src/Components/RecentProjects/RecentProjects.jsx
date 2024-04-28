e6a2d3f259d39fd165c0317538f78c619f494766
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-28 20:47:36 +0600

    completed one recent project

--- src/Components/RecentProjects/RecentProjects.jsx
+++ src/Components/RecentProjects/RecentProjects.jsx
@@ -9,20 +9,20 @@ const RecentProjects = () => {
         {/* div to carry all three cards for projects */}
         <div>
           {/* first project */}
-          <div className="p-5 rounded-md bg-[#1E293B]">
+          <div className="p-8 rounded-md bg-[#1E293B]">
             <div className="flex gap-4 justify-start rounded-md items-center">
               <img
                 className="w-20"
                 src="https://i.ibb.co/d5hj4FG/restaurant.png"
                 alt=""
               />
-              <div>
-                <div className="flex items-center gap-3">
+              <div className="ms-8">
+                <div className="flex items-center gap-3 mb-4">
                   <h4 className="text-2xl font-semibold">Bistro boss Restaurent</h4>
-                  <p className="bg-[#E879F9] rounded-md text-xs text-[#701A75] p-1">Firebase</p>
-                  <p>Node</p>
-                  <p>React</p>
-                  <p>Mongodb</p>
+                  <p className="bg-[#E879F9] rounded-md font-medium text-xs text-[#701A75] p-1">Firebase</p>
+                  <p className="bg-[#A3E635] rounded-md font-medium text-xs text-[#365316] p-1">Node</p>
+                  <p className="bg-[#38BDF8] rounded-md font-medium text-xs text-[#0C589A] p-1">React</p>
+                  <p className="bg-[#FB7185] rounded-md font-medium text-xs text-[#9E1C37] p-1">Mongodb</p>
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p></div>
             </div>
