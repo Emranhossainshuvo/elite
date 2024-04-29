c24a912bcade2375b3c540e110e7d7ccfbe59019
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-29 16:13:32 +0600

    created skills component and connected it with the home component

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,16 @@
+
+const Skills = () => {
+    return (
+        <>
+            <div>
+                <h3>My Skills</h3>          
+                {/* a div for contain my all skills */}
+                <div>
+                    
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Skills;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../Components/Banner/Banner";
 import RecentProjects from "../../Components/RecentProjects/RecentProjects";
+import Skills from "../../Components/Skills/Skills";
 
 const Home = () => {
     return (
         <div className="mt-12">
             <Banner></Banner>
             <RecentProjects></RecentProjects>
+            <Skills></Skills>
             <h3>This is home page</h3>
         </div>
     );
