d08fc7d48a44ced22e3bf055778d39896454317d
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-29 16:04:02 +0600

    completed all three projects

--- src/Components/RecentProjects/RecentProjects.jsx
+++ src/Components/RecentProjects/RecentProjects.jsx
@@ -13,28 +13,6 @@ const RecentProjects = () => {
             <div className="md:flex gap-4 justify-start rounded-md items-center">
               <img
                 className="md:w-20 mb-3 w-28 mx-auto"
-                src="https://i.ibb.co/d5hj4FG/restaurant.png"
-                alt=""
-              />
-              <div className="md:ms-8">
-                <div className="md:flex items-center gap-3 mb-4">
-                  <h4 className="text-2xl font-semibold">Bistro boss Restaurent</h4>
-                  <ul className="flex gap-3 text-center">
-                    <li className="bg-[#E879F9] rounded-md w-16 font-medium text-xs text-[#701A75] p-1">Firebase</li>
-                    <li className="bg-[#A3E635] rounded-md w-16 font-medium text-xs text-[#365316] p-1">Node</li>
-                    <li className="bg-[#38BDF8] rounded-md w-16 font-medium text-xs text-[#0C589A] p-1">React</li>
-                    <li className="bg-[#FB7185] rounded-md w-16 font-medium text-xs text-[#9E1C37] p-1">Mongodb</li>
-                  </ul>
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p></div>
-            </div>
-
-          </div>
-          {/* second project */}
-          <div className="p-8 mt-4 rounded-md bg-[#1E293B]">
-            <div className="md:flex gap-4 justify-start rounded-md items-center">
-              <img
-                className="md:w-20 mb-3 w-28 mx-auto"
                 src="https://i.ibb.co/Hzn23c8/healthcare.png"
                 alt=""
               />
@@ -48,7 +26,29 @@ const RecentProjects = () => {
                     <li className="bg-[#FB7185] rounded-md w-16 font-medium text-xs text-[#9E1C37] p-1">Mongodb</li>
                   </ul>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p></div>
+                <p>Me and my friends develop this project with the help of MERN stack. I spent lots of hours to make this smooth and user friendly also we added animations.</p></div>
+            </div>
+
+          </div>
+          {/* second project */}
+          <div className="p-8 mt-4 rounded-md bg-[#1E293B]">
+            <div className="md:flex gap-4 justify-start rounded-md items-center">
+              <img
+                className="md:w-20 mb-3 w-28 mx-auto"
+                src="https://i.ibb.co/d5hj4FG/restaurant.png"
+                alt=""
+              />
+              <div className="md:ms-8">
+                <div className="md:flex items-center gap-3 mb-4">
+                  <h4 className="text-2xl font-semibold">Bistro boss Restaurent</h4>
+                  <ul className="flex gap-3 text-center">
+                    <li className="bg-[#E879F9] rounded-md w-16 font-medium text-xs text-[#701A75] p-1">Firebase</li>
+                    <li className="bg-[#A3E635] rounded-md w-16 font-medium text-xs text-[#365316] p-1">Node</li>
+                    <li className="bg-[#38BDF8] rounded-md w-16 font-medium text-xs text-[#0C589A] p-1">React</li>
+                    <li className="bg-[#FB7185] rounded-md w-16 font-medium text-xs text-[#9E1C37] p-1">Mongodb</li>
+                  </ul>
+                </div>
+                <p>It is an online based restaurant where people can get different types of food and enjoy new dishes. It is a single page application and have payment system through Strip</p></div>
             </div>
 
           </div>
@@ -64,13 +64,12 @@ const RecentProjects = () => {
                 <div className="md:flex items-center gap-3 mb-4">
                   <h4 className="text-2xl font-semibold">Tik tak toe</h4>
                   <ul className="flex gap-3 text-center">
-                    <li className="bg-[#E879F9] rounded-md w-16 font-medium text-xs text-[#701A75] p-1">Firebase</li>
-                    <li className="bg-[#A3E635] rounded-md w-16 font-medium text-xs text-[#365316] p-1">Node</li>
-                    <li className="bg-[#38BDF8] rounded-md w-16 font-medium text-xs text-[#0C589A] p-1">React</li>
-                    <li className="bg-[#FB7185] rounded-md w-16 font-medium text-xs text-[#9E1C37] p-1">Mongodb</li>
+                    <li className="bg-[#E879F9] rounded-md w-16 font-medium text-xs text-[#701A75] p-1">HTML</li>
+                    <li className="bg-[#A3E635] rounded-md w-16 font-medium text-xs text-[#365316] p-1">CSS</li>
+                    <li className="bg-[#38BDF8] rounded-md w-16 font-medium text-xs text-[#0C589A] p-1">JavaScript</li>
                   </ul>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p></div>
+                <p>It was a fun project I developed this game by watching a you tube video although I edited more later. During this project I learnt many new things in both JavaScript and Css</p></div>
             </div>
 
           </div>
