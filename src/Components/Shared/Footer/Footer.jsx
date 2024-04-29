16695c6e43e483dc944e0c5ca0651694b812e143
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-29 21:57:04 +0600

    added momnet js to show dynamic year in the footer

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,3 +1,6 @@
+import moment from 'moment';
+
+
 
 const Footer = () => {
     return (
@@ -5,7 +8,7 @@ const Footer = () => {
         <hr className="mx-2 my-4" />
             <footer className="footer ps-2 text-base-content">
                 <aside>
-                    <p className="text-white">© Copyright  2024 - All right reserved by ACME Industries Ltd</p>
+                    <p className="text-white">© Copyright {moment().format('YYYY')} - All right reserved by Md. Emran Hossain</p>
                 </aside>
             </footer>
         </>
