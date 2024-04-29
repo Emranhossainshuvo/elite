635233384ebf9deba20a3fbf15a40015fe5fddde
EmranHossain <emran.shuvo.dev@gmail.com>
2024-04-29 16:35:00 +0600

    added skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -2,11 +2,68 @@
 const Skills = () => {
     return (
         <>
-            <div>
-                <h3>My Skills</h3>          
+            <div className="mx-2">
+                <h2 className="text-3xl font-semibold ">
+                    My <span className="text-[#1AC0EC]">skills</span>{" "}
+                </h2>
                 {/* a div for contain my all skills */}
-                <div>
-                    
+                <div className="md:grid md:grid-cols-2 gap-x-3 ">
+                    <div>
+                        <div className="flex justify-between items-center">
+                            <h4>HTML</h4>
+                            <p>70%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="70" max="100"></progress>
+                    </div>
+                    <div  >
+                        <div className="flex justify-between items-center">
+                            <h4>CSS</h4>
+                            <p>60%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="60" max="100"></progress>
+                    </div>
+                    <div  >
+                        <div className="flex justify-between items-center">
+                            <h4>JavaScript</h4>
+                            <p>40%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="40" max="100"></progress>
+                    </div>
+                    <div  >
+                        <div className="flex justify-between items-center">
+                            <h4>TailwindCSS</h4>
+                            <p>80%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="80" max="100"></progress>
+                    </div>
+                    <div  >
+                        <div className="flex justify-between items-center">
+                            <h4>React</h4>
+                            <p>60%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="60" max="100"></progress>
+                    </div>
+                    <div  >
+                        <div className="flex justify-between items-center">
+                            <h4>Node</h4>
+                            <p>50%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="50" max="100"></progress>
+                    </div>
+                    <div  >
+                        <div className="flex justify-between items-center">
+                            <h4>Express</h4>
+                            <p>40%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="40" max="100"></progress>
+                    </div>
+                    <div  >
+                        <div className="flex justify-between items-center">
+                            <h4>Mongodb</h4>
+                            <p>60%</p>
+                        </div>
+                        <progress className="progress progress-info w-full" value="60" max="100"></progress>
+                    </div>
                 </div>
             </div>
         </>
