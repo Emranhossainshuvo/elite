f36dc8151495d67c8a0696571a23ef571856f827
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-01 11:03:34 +0600

    added all three cards for education

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -4,15 +4,15 @@ const Education = () => {
         <>
             <div className="mx-2">
 
-                <h2 className="text-3xl font-semibold ">
+                <h2 className="text-3xl my-4 font-semibold ">
                     My <span className="text-[#1AC0EC]">Education</span>{" "}
                 </h2>
 
                 <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3 w-[40%]">
-                    <p className="text-[#38BDF8]">2024 - Running</p>
-                    <h3 className="text-xl text-white font-medium">Bs.c Geography</h3>
-                    <p className="font-extralight">Government Titumir College</p>
-                    <p className="text-base text-white">Currently pursuing a Bachelor of Science degree at Government Titumir College Dhaka.</p>
+                    <p className="text-[#38BDF8]">2023 - 2024</p>
+                    <h3 className="text-xl text-white font-medium">Web development</h3>
+                    <p className="font-extralight">Programmine Hero</p>
+                    <p className="text-base text-white">I did a  online professional course for around 8 month with Programming hero</p>
                 </div>
                 <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3 w-[40%]">
                     <p className="text-[#38BDF8]">2024 - Running</p>
