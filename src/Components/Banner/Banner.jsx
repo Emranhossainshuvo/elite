f2684c9c6d5a2899a60299cd34c1335802cd369c
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-01 12:49:03 +0600

    fixed some responsiveness issues fron the banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,15 +3,15 @@ const Banner = () => {
     <>
       <div className="md:flex justify-between items-center">
         <div className="md:w-1/2">
-          <h3 className=" text-3xl font-medium pb-5">
+          <h3 className=" text-3xl font-medium ms-1 pb-5">
             <span className="text-white">Hello, I&apos;m Md.{" "}</span>
             <span className="text-[#1AC0EC]">Emran Hossain</span>
           </h3>
-          <p className="text-lg text-white font-medium">
+          <p className="text-lg text-white font-medium ms-1">
             I&apos; m a Junior MERN stack developer. I love to create dynamic full stack web applications. I love the way I bring life in an web application.I believe in smoother experience
           </p>
-          <ul className="flex gap-5 mt-10">
-            <li className="w-10">
+          <ul className="flex md:w-2/4 w-4/6 md:my-10 md:mx-1 mx-auto  gap-5 mt-10">
+            <li className=" ">
               <a target="_blank"
                 href="https://www.linkedin.com/in/md-emran-hossain-shuvo/"
                 title="linkedin icon"
@@ -20,7 +20,7 @@ const Banner = () => {
               </a>
             </li>
 
-            <li className="w-10">
+            <li className=" ">
               <a target="_blank"
                 href="https://twitter.com/Shuvo0031770590"
                 title="twitter icon"
@@ -29,7 +29,7 @@ const Banner = () => {
               </a>
             </li>
 
-            <li className="w-10">
+            <li className=" ">
               <a target="_blank"
 
                 href="https://github.com/Emranhossainshuvo"
@@ -39,7 +39,7 @@ const Banner = () => {
               </a>
             </li>
 
-            <li className="w-10">
+            <li className=" ">
               <a target="_blank"
                 href="https://medium.com/@emran.shuvo.dev"
                 title="medium icon"
@@ -50,7 +50,7 @@ const Banner = () => {
           </ul>
         </div>
         <div>
-          <img className="w-5/6"
+          <img className="w-5/6 mx-auto"
             src="https://i.ibb.co/3rhzz4M/IMG-20231209-080753-removebg-4-removebg-preview.png"
             alt=""
           />
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ const Navbar = () => {
     <>
       <div className="md:flex justify-between text-xl font-medium pt-5 items-center">
         <div>
-            <p className="text-[#0891B2]">Md. Emran Hossain</p>
+            <p className="text-[#0891B2] my-2 ms-1">Md. Emran Hossain</p>
         </div>
         <div className="text-white">
-            <ul className="flex items-center gap-3">
+            <ul className="flex items-center ms-1 gap-3">
                 <li><a target="blank" href="https://medium.com/@emran.shuvo.dev">Blogs</a></li>
                 <li className="bg-[#38BDF8] p-1 rounded-md text-white"><a href="/public/Emran.hossain.developer.pdf" download>Resume</a></li>
             </ul>
