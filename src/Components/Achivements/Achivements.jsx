3690c11e0af5fdd1ebcbb021095dd7a6828df616
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-01 20:01:41 +0600

    added another achivement and link it to direct file

--- src/Components/Achivements/Achivements.jsx
+++ src/Components/Achivements/Achivements.jsx
@@ -11,13 +11,18 @@ const Achivements = () => {
                 <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">December 2023</p>
                     <h3 className="text-xl text-white font-medium">Web development Certificate</h3>
-                    
+
                     <p className="text-base text-white">I&apos;ve got certificate for completig my web development professional course with programming hero. Here&apos;s the <span className="text-[#38BDF8] "><a href="/public/certificate.pdf" download>Certificate</a></span></p>
                 </div>
                 <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">Mars 2024</p>
                     <h3 className="text-xl text-white font-medium">Recommendation Letter</h3>
                     <p className="text-base text-white">I was part fo two month long Programming-Hero endgame programm It&apos;s like a shadow internship. Here&apos;s the <a className="text-[#38BDF8]" href="/public/Md. Emran Hossain.pdf" download>Recommendation Letter</a> </p>
+                </div>
+                <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3">
+                    <p className="text-[#38BDF8]">February 2024</p>
+                    <h3 className="text-xl text-white font-medium">Campus Ambassador</h3>
+                    <p className="text-base text-white">Currently I am a Campus ambassador at Government Titumir College of Programming Hero. Here my selection <a className="text-[#38BDF8]" download href="/public/Emran Hossain(ps) (1).png">Banner</a>  </p>
                 </div>
             </div>
         </>
