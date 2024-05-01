d1daed8df17a64e47b69bf3fb1bb186bc14e1446
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-01 19:31:46 +0600

    made educational institution names dynamic

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -11,19 +11,19 @@ const Education = () => {
                 <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">2023 - 2024</p>
                     <h3 className="text-xl text-white font-medium">Web development</h3>
-                    <p className="font-extralight">Programmine Hero</p>
+                    <p className="font-extralight"><a target="_blank" href="https://web.programming-hero.com/">Programmine Hero</a></p>
                     <p className="text-base text-white">I did a  online professional course for around 8 month with Programming hero</p>
                 </div>
                 <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">2024 - Running</p>
                     <h3 className="text-xl text-white font-medium">Bs.c Geography</h3>
-                    <p className="font-extralight">Government Titumir College</p>
+                    <p className="font-extralight"><a href="https://titumircollege.gov.bd/">Government Titumir College</a></p>
                     <p className="text-base text-white">Currently pursuing a Bachelor of Science degree at Government Titumir College Dhaka.</p>
                 </div>
                 <div className="bg-[#1E293B] ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">2020 - 2022</p>
                     <h3 className="text-xl text-white font-medium">Higher Secondary Certificate</h3>
-                    <p className="font-extralight">Hajiganj Model Govt. College</p>
+                    <p className="font-extralight"><a href="https://hmgovcollege.com/">Hajiganj Model Govt. College</a></p>
                     <p className="text-base text-white">I completed my HSC at department of Humanities with CGPA of 4.94 out of 5</p>
                 </div>
             </div>
