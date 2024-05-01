1bcfc15b6276aa4f916b22d83826bcb811578fa6
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-01 11:07:50 +0600

    added cardf for achivements

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -8,19 +8,19 @@ const Education = () => {
                     My <span className="text-[#1AC0EC]">Education</span>{" "}
                 </h2>
 
-                <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3 w-[40%]">
+                <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">2023 - 2024</p>
                     <h3 className="text-xl text-white font-medium">Web development</h3>
                     <p className="font-extralight">Programmine Hero</p>
                     <p className="text-base text-white">I did a  online professional course for around 8 month with Programming hero</p>
                 </div>
-                <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3 w-[40%]">
+                <div className="bg-[#1E293B] mb-2 ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">2024 - Running</p>
                     <h3 className="text-xl text-white font-medium">Bs.c Geography</h3>
                     <p className="font-extralight">Government Titumir College</p>
                     <p className="text-base text-white">Currently pursuing a Bachelor of Science degree at Government Titumir College Dhaka.</p>
                 </div>
-                <div className="bg-[#1E293B] ps-4 rounded-md py-3 w-[40%]">
+                <div className="bg-[#1E293B] ps-4 rounded-md py-3">
                     <p className="text-[#38BDF8]">2020 - 2022</p>
                     <h3 className="text-xl text-white font-medium">Higher Secondary Certificate</h3>
                     <p className="font-extralight">Hajiganj Model Govt. College</p>
