076f342baf0c3333f2ac1ace771e0e74b03ed6ee
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-02 20:15:56 +0600

    added custom colors in tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'custom-bg': '#F4E3D7',
+      'custom-text': '#4A3933',
+      'custom-sp': '#7E9363',
+    },
   },
   plugins: [require("daisyui")],
 }
